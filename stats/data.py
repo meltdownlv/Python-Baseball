675f0aa15740fddb5e7a40bc242f22a06fdad9de
Sakae Watanabe <meltdownlv@gmail.com>
2021-04-21 21:23:10 -0700

    Check module 2 work

--- stats/data.py
+++ stats/data.py
@@ -24,4 +24,4 @@ games = pd.concat([games, identifiers], axis=1, sort=False)
 games = games.fillna(' ')
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
 
-print(games.head())
+# print(games.head())
